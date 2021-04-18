f4d1b1cc705e80ef72735e72703d92a19f69e174
Daniel <harel_daniel@yahoo.com>
2021-04-18 11:59:45 +0300

    restructured folders for deployment

--- server/client/src/actions/types.ts
+++
@@ -1,8 +0,0 @@
-import { FetchUserAction, FetchSurveysAction } from './index';
-
-export enum ActionTypes {
-    FETCH_USER = 'fetch_user',
-    FETCH_SURVEYS = 'fetch_surveys'
-}
-
-export type Action = FetchUserAction | FetchSurveysAction;
--- server/client/src/components/surveys/formFields.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-// this is a configuration array of objects that define the fields in a form.
-export default [
-    {label: "Survey Title", name: "title"},
-    {label: "Subject Line", name: "subject"},
-    {label: "Email Body", name: "body"},
-    {label: "Recipient List", name: "recipients"}
-];
